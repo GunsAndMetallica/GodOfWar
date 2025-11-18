0bb3ea0d3bc71de76b81e219d8a6925349e8ffcd
Rayaan Verma <rayaanverma1504@gmail.com>
2025-11-18 20:16:54 +0530

    Update PlayerCharacter.cpp

--- PlayerCharacter.cpp
+++ PlayerCharacter.cpp
@@ -1,48 +1,159 @@
 #include "PlayerCharacter.h"
-#include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
-#include "WeaponBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CombatComponent.h"
+#include "RageMode.h"
+#include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
 
-APlayerCharacter::APlayerCharacter() {
+APlayerCharacter::APlayerCharacter()
+{
     PrimaryActorTick.bCanEverTick = true;
-    Health = 100.f;
-    MaxHealth = 100.f;
+
+    /* -------- CAMERA SETUP -------- */
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArm->SetupAttachment(RootComponent);
+    SpringArm->TargetArmLength = 350.f;           // Over-shoulder distance
+    SpringArm->bUsePawnControlRotation = true;
+    SpringArm->SocketOffset = FVector(0.f, 60.f, 60.f);
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+    Camera->bUsePawnControlRotation = false;
+
+    /* -------- MOVEMENT SETTINGS -------- */
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+    GetCharacterMovement()->JumpZVelocity = 600.f;
+    GetCharacterMovement()->AirControl = 0.35f;
+
+    /* -------- COMBAT + RAGE -------- */
+    CombatComp = CreateDefaultSubobject<UCombatComponent>(TEXT("CombatComponent"));
+    RageComp = CreateDefaultSubobject<URageMode>(TEXT("RageComponent"));
 }
 
-void APlayerCharacter::BeginPlay() {
+void APlayerCharacter::BeginPlay()
+{
     Super::BeginPlay();
 }
 
-void APlayerCharacter::Tick(float DeltaTime) {
+void APlayerCharacter::Tick(float DeltaTime)
+{
     Super::Tick(DeltaTime);
 }
 
-void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
     Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    /* -------- MOVEMENT -------- */
     PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
     PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
     PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::Turn);
     PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::LookUp);
 
-    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &APlayerCharacter::JumpAction);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &APlayerCharacter::StartJump);
+    PlayerInputComponent->BindAction("Jump", IE_Released, this, &APlayerCharacter::StopJump);
     PlayerInputComponent->BindAction("Dodge", IE_Pressed, this, &APlayerCharacter::Dodge);
-    PlayerInputComponent->BindAction("AttackAxe", IE_Pressed, this, &APlayerCharacter::AttackAxe);
-    PlayerInputComponent->BindAction("ThrowAxe", IE_Pressed, this, &APlayerCharacter::ThrowAxe);
-    PlayerInputComponent->BindAction("RecallAxe", IE_Pressed, this, &APlayerCharacter::RecallAxe);
-    PlayerInputComponent->BindAction("AttackBlades", IE_Pressed, this, &APlayerCharacter::AttackBlades);
-    PlayerInputComponent->BindAction("Block", IE_Pressed, this, &APlayerCharacter::Block);
-    PlayerInputComponent->BindAction("RageMode", IE_Pressed, this, &APlayerCharacter::RageMode);
+
+    /* -------- COMBAT -------- */
+    PlayerInputComponent->BindAction("LightAttack", IE_Pressed, this, &APlayerCharacter::LightAttack);
+    PlayerInputComponent->BindAction("HeavyAttack", IE_Pressed, this, &APlayerCharacter::HeavyAttack);
+    PlayerInputComponent->BindAction("ThrowWeapon", IE_Pressed, this, &APlayerCharacter::ThrowWeapon);
+    PlayerInputComponent->BindAction("RecallWeapon", IE_Pressed, this, &APlayerCharacter::RecallWeapon);
+
+    PlayerInputComponent->BindAction("Block", IE_Pressed, this, &APlayerCharacter::BlockPressed);
+    PlayerInputComponent->BindAction("Block", IE_Released, this, &APlayerCharacter::BlockReleased);
+
+    PlayerInputComponent->BindAction("Rage", IE_Pressed, this, &APlayerCharacter::ActivateRage);
 }
 
-void APlayerCharacter::MoveForward(float Value) { AddMovementInput(GetActorForwardVector(), Value); }
-void APlayerCharacter::MoveRight(float Value) { AddMovementInput(GetActorRightVector(), Value); }
-void APlayerCharacter::Turn(float Value) { AddControllerYawInput(Value); }
-void APlayerCharacter::LookUp(float Value) { AddControllerPitchInput(Value); }
-void APlayerCharacter::JumpAction() { Jump(); }
-void APlayerCharacter::Dodge() { /* Add dodge animation + invulnerability */ }
-void APlayerCharacter::AttackAxe() { /* Play axe attack animation + detect hit */ }
-void APlayerCharacter::ThrowAxe() { /* Spawn axe projectile */ }
-void APlayerCharacter::RecallAxe() { /* Move axe back to player */ }
-void APlayerCharacter::AttackBlades() { /* Play dual blade combo */ }
-void APlayerCharacter::Block() { /* Reduce damage */ }
-void APlayerCharacter::RageMode() { /* Activate special attacks */ }
+/* ========================================================
+   MOVEMENT
+======================================================== */
+
+void APlayerCharacter::MoveForward(float Value)
+{
+    if (Value != 0.f)
+        AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+    if (Value != 0.f)
+        AddMovementInput(GetActorRightVector(), Value);
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+    AddControllerYawInput(Value * TurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::LookUp(float Value)
+{
+    AddControllerPitchInput(Value * LookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::StartJump()
+{
+    Jump();
+}
+
+void APlayerCharacter::StopJump()
+{
+    StopJumping();
+}
+
+void APlayerCharacter::Dodge()
+{
+    if (!CombatComp) return;
+    CombatComp->Dodge();
+}
+
+/* ========================================================
+   COMBAT
+======================================================== */
+
+void APlayerCharacter::LightAttack()
+{
+    if (!CombatComp) return;
+    CombatComp->PerformLightAttack();
+}
+
+void APlayerCharacter::HeavyAttack()
+{
+    if (!CombatComp) return;
+    CombatComp->PerformHeavyAttack();
+}
+
+void APlayerCharacter::ThrowWeapon()
+{
+    if (!CombatComp) return;
+    CombatComp->ThrowWeapon();
+}
+
+void APlayerCharacter::RecallWeapon()
+{
+    if (!CombatComp) return;
+    CombatComp->RecallWeapon();
+}
+
+void APlayerCharacter::BlockPressed()
+{
+    if (!CombatComp) return;
+    CombatComp->StartBlocking();
+}
+
+void APlayerCharacter::BlockReleased()
+{
+    if (!CombatComp) return;
+    CombatComp->StopBlocking();
+}
+
+void APlayerCharacter::ActivateRage()
+{
+    if (!RageComp) return;
+    RageComp->ActivateRageMode();
+}
+
